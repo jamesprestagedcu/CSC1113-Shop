0cec57901136bd89c062fcb4bb2465ec6303ed20
James Prestage <prestaj2@l125-24.maas>
2025-09-15 13:28:21 +0100

    Week 2 - Products and Individual Products

--- jamesshop/urls.py
+++ jamesshop/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('', views.index, name="index")
-] 
+   path('', views.index, name="index"),
+   path('products/', views.all_products, name='all_products'),
+   path('products/<int:prodid>/', views.product_individual, name='individual_product')
+]
--- jamesshop/views.py
+++ jamesshop/views.py
@@ -1,9 +1,14 @@
-from django.shortcuts import render
-
-# Create your views here.
-
+from django.shortcuts import render, get_object_or_404
+from .models import *
 from django.http import HttpResponse
-from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+
+def all_products(request):
+    products = Product.objects.all()
+    return render(request, 'products.html', {'products': products})
+
+def product_individual(request, prodid):
+    product = get_object_or_404(Product, id=prodid)
+    return render(request, 'individual_product.html', {'product': product})
